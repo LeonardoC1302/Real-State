4a3d7201954f54ce69e48c292cdfd74543832888
LeonardoC1302 <lecespedes@estudiantec.cr>
2023-05-28 10:47:12 -0600

    Dark Mode

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,9 @@
 document.addEventListener('DOMContentLoaded', () => {
     eventListeners();
+    darkMode();
 });
 
+// Responsive Menu
 function eventListeners() {
     const mobileMenu = document.querySelector('.mobile-menu');
     mobileMenu.addEventListener('click', responsiveMenu);
@@ -10,4 +12,14 @@ function eventListeners() {
 function responsiveMenu() {
     const navigation = document.querySelector('.navigation');
     navigation.classList.toggle('show'); // toggle: si tiene la clase la quita, si no la tiene la agrega
+}
+
+// Dark Mode
+function darkMode() {
+    const darkMode = document.querySelector('.dark-mode-button');
+
+    darkMode.addEventListener('click', () => {
+        console.log('click');
+        document.body.classList.toggle('dark-mode');
+    });
 }
